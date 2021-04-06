cc48475f11b23ab20ead8daf974432bbee701864
Justin Balden <justin.f.balden@gmail.com>
2021-04-06 02:26:45 -0400

    Frontend build

---
+++ build/precache-manifest.d849d66a3b64627e992fe481cfe30d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a580917eec9662236dab3526248f164a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f2b5120e4c8e299d80c",
+    "url": "/static/css/main.8961d9a5.chunk.css"
+  },
+  {
+    "revision": "9fe19a6ade9f614fcd0f",
+    "url": "/static/js/2.595e3fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.595e3fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2b5120e4c8e299d80c",
+    "url": "/static/js/main.1987463e.chunk.js"
+  },
+  {
+    "revision": "7b750ca7d70f30d2f5fe",
+    "url": "/static/js/runtime-main.7686cdbb.js"
+  }
+]);
